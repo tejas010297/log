e8cf1ffdf65b99b0637f45e180abf7d0abd37a96
yashkpatel <ykp.2604@gmail.com>
2017-10-04 11:36:31 +0530

    Add files via upload

---
+++ userProfile.java
@@ -0,0 +1,19 @@
+package com.example.tejaspatel.log;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.tejaspatel.log.*;
+
+/**
+ * Created by Dell on 02-10-2017.
+ */
+
+public class userProfile extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+
+    }
+}
